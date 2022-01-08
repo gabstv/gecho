01bda8fa34dc70bd04b1f61645274ac8601770ae
Gabriel Ochsenhofer <gabs@gabs.tv>
2022-01-08 10:31:10 -0300

    added other methods

--- get.go
+++ get.go
@@ -17,3 +17,59 @@ func Get[ReqT, ResT any](e *echo.Echo, path string, fn func(c echo.Context, req 
 		return c.JSON(200, res)
 	}, m...)
 }
+
+func Post[ReqT, ResT any](e *echo.Echo, path string, fn func(c echo.Context, req ReqT) (ResT, error), m ...echo.MiddlewareFunc) *echo.Route {
+	return e.POST(path, func(c echo.Context) error {
+		var rq ReqT
+		if err := c.Bind(&rq); err != nil {
+			return err
+		}
+		res, err := fn(c, rq)
+		if err != nil {
+			return err
+		}
+		return c.JSON(200, res)
+	}, m...)
+}
+
+func Put[ReqT, ResT any](e *echo.Echo, path string, fn func(c echo.Context, req ReqT) (ResT, error), m ...echo.MiddlewareFunc) *echo.Route {
+	return e.PUT(path, func(c echo.Context) error {
+		var rq ReqT
+		if err := c.Bind(&rq); err != nil {
+			return err
+		}
+		res, err := fn(c, rq)
+		if err != nil {
+			return err
+		}
+		return c.JSON(200, res)
+	}, m...)
+}
+
+func Patch[ReqT, ResT any](e *echo.Echo, path string, fn func(c echo.Context, req ReqT) (ResT, error), m ...echo.MiddlewareFunc) *echo.Route {
+	return e.PATCH(path, func(c echo.Context) error {
+		var rq ReqT
+		if err := c.Bind(&rq); err != nil {
+			return err
+		}
+		res, err := fn(c, rq)
+		if err != nil {
+			return err
+		}
+		return c.JSON(200, res)
+	}, m...)
+}
+
+func Delete[ReqT, ResT any](e *echo.Echo, path string, fn func(c echo.Context, req ReqT) (ResT, error), m ...echo.MiddlewareFunc) *echo.Route {
+	return e.DELETE(path, func(c echo.Context) error {
+		var rq ReqT
+		if err := c.Bind(&rq); err != nil {
+			return err
+		}
+		res, err := fn(c, rq)
+		if err != nil {
+			return err
+		}
+		return c.JSON(200, res)
+	}, m...)
+}
